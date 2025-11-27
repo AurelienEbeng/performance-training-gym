4bb7ea9de6115c278e4b55c07c96d49b75e9958e
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 06:41:26 -0500

    The list of navbar items opens and closes smoothly

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 const Navbar = () => {
-  const [openMenu, setOpenMenu] = useState(false);
-  const icon = openMenu ? "multiply" : "plus";
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const icon = isMenuOpen ? "multiply" : "plus";
   const navbarItems = [
     { href: "/", name: "home" },
     { href: "/about", name: "about" },
     { href: "/reserve", name: "reserve your spot" },
   ];
+  const navbarUlMarginTop = isMenuOpen ? "mt-0" : "-mt-100";
   return (
     <>
       <div className="flex justify-between px-4 bg-[#E9ECFF] items-center">
@@ -19,17 +20,18 @@ const Navbar = () => {
         </div>
         <div
           className={`${icon}-icon w-18 h-18`}
-          onClick={() => setOpenMenu((prev) => !prev)}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
         />
       </div>
-      {openMenu &&
-        navbarItems.map((item) => (
-          <ul className="uppercase bg-[#E9ECFF] pb-2 text-center z-100 ">
-            <li key={item} >
-              <a href={item.href}>{item.name}</a>
-            </li>
-          </ul>
-        ))}
+      {navbarItems.map((item) => (
+        <ul
+          className={`uppercase bg-[#E9ECFF] pb-2 text-center z-100 ${navbarUlMarginTop} transition-all duration-500 ease-in-out`}
+        >
+          <li key={item}>
+            <a href={item.href}>{item.name}</a>
+          </li>
+        </ul>
+      ))}
     </>
   );
 };
