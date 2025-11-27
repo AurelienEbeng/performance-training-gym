b6832af0d9574fc9a524590d42c13ef93ad958c2
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 11:54:52 -0500

    Finished mobile version of the hero section

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,21 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <div className="text-4xl md:text-6xl lg:text-8xl text-center font-extrabold text-gray-900 uppercase font-anek-tamil py-4 border">
-        Train Hard. <span className="text-[#808CFD]">Live Better</span>
+    <>
+      <div className="text-center text-gray-900 py-4 border-y headline-one">
+        Train Hard. <span className="text-color-one">Live Better</span>
       </div>
-    </div>
+      <img src="/images/heroImage.png" className=" w-full object-cover" />
+      <div className="bg-color-two px-8">
+        <div className="uppercase headline-two pt-4">FOR THE COMMITTED</div>
+        <p className="paragraph-two">
+          Train like an athlete with top-tier equipment and expert programming.
+          Whether you're building muscle or breaking PRs, we help you push past
+          limits.
+        </p>
+        <button className="links-button py-8 pl-4">About us</button>
+      </div>
+    </>
   );
 };
 
