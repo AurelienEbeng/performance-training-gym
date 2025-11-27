7e9f0ff45869862a0863a549f7bc618ec59c6503
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-26 23:17:04 -0500

    Made the Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,37 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { useState } from "react";
 
-export default Navbar
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const icon = openMenu ? "multiply" : "plus";
+  const navbarItems = [
+    { href: "/", name: "home" },
+    { href: "/about", name: "about" },
+    { href: "/reserve", name: "reserve your spot" },
+  ];
+  return (
+    <>
+      <div className="flex justify-between px-4 bg-[#E9ECFF] items-center">
+        <div className="flex items-center">
+          <img src="/vector.svg" />
+          <div className="font-instrument-sans font-bold text-xl pl-2">
+            PrimalTraining
+          </div>
+        </div>
+        <div
+          className={`${icon}-icon w-18 h-18`}
+          onClick={() => setOpenMenu((prev) => !prev)}
+        />
+      </div>
+      {openMenu &&
+        navbarItems.map((item) => (
+          <ul className="uppercase bg-[#E9ECFF] pb-2 text-center z-100 ">
+            <li key={item} >
+              <a href={item.href}>{item.name}</a>
+            </li>
+          </ul>
+        ))}
+    </>
+  );
+};
+
+export default Navbar;
