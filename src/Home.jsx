be6e8e58d05025d76c9933924fe37f0b53f686d3
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 10:45:35 -0500

    Made hero headline

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="text-4xl font-bold text-gray-900">Train Hard. Live Better</div>
+      <div className="text-4xl md:text-6xl lg:text-8xl text-center font-extrabold text-gray-900 uppercase font-anek-tamil py-4 border">
+        Train Hard. <span className="text-[#808CFD]">Live Better</span>
+      </div>
     </div>
   );
 };
