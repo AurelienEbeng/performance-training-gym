1fb67f302159d69f3bb967da6f8ccd9b3119900a
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 16:50:07 -0500

    Finished mobile version of the mission section. Added the fadeIn animation on scroll

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import ScrollFadeIn from "./ScrollFadeIn";
 
 const Home = () => {
   return (
@@ -6,6 +7,7 @@ const Home = () => {
       <div className="text-center text-gray-900 py-4 border-y headline-one">
         Train Hard. <span className="text-color-one">Live Better</span>
       </div>
+      {/* Hero section */}
       <div className="md:flex">
         <img
           src="/images/heroImage.png"
@@ -24,6 +26,29 @@ const Home = () => {
             <Link to="/about">About us</Link>
           </button>
         </div>
+      </div>
+      {/* Mission section */}
+      <ScrollFadeIn>
+        <img
+          src="/images/mission.png"
+          className=" w-full md:w-1/3 object-cover"
+        />
+      </ScrollFadeIn>
+      <div className="bg-color-two p-8">
+        <div className="headline-two">Guided by experts</div>
+        <p className="paragraph-two">
+          We believe in creating a positive environment where you can thrive.
+          We're here to help you achieve your goals and unlock your full
+          potential.
+        </p>
+      </div>
+      <div className="bg-color-two p-8 border-y">
+        <div className="headline-two">DYNAMIC OPEN GYM</div>
+        <p className="paragraph-two">
+          Our facility is the optimal environment for strength training and
+          performance, fully equipped with top-of-the-line tools, ample training
+          areas, and a focus on functional movement.
+        </p>
       </div>
     </>
   );
---
+++ src/ScrollFadeIn.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+const ScrollFadeIn = ({ children }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // if section is on the viewport
+        if (entry.isIntersecting && ref.current) {
+          ref.current.classList.add("animate-fade-in");
+        } else {
+          ref.current.classList.remove("animate-fade-in");
+        }
+      },
+      { threshold: 0.1 }
+    );
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div ref={ref} className="opacity-0">
+      {children}
+    </div>
+  );
+};
+
+export default ScrollFadeIn;
