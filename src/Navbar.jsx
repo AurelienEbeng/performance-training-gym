68a06241f49bab2d3f9ca3e82c92a911b3ed410c
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 07:54:10 -0500

    Made the logo a link and reserve your spot button follows the design

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,32 +12,48 @@ const Navbar = () => {
   return (
     <>
       <div className="font-instrument-sans flex justify-between px-4 bg-[#E9ECFF] items-center md:py-5">
-        <div className="flex items-center">
+        <a href="/" className="flex items-center">
           <img src="/vector.svg" />
-          <div className="font-bold text-xl pl-2">
-            PrimalTraining
-          </div>
-        </div>
+          <div className="font-bold text-xl pl-2">PrimalTraining</div>
+        </a>
         <ul
-            className={`max-md:hidden uppercase bg-[#E9ECFF] pb-2 text-center flex gap-4 pr-4`}
-          >
-            {navbarItems.map((item) => (
-              <li key={item.href}>
-                <a href={item.href} className="hover:bg-gray-300 p-2 rounded-lg">{item.name}</a>
-              </li>
-            ))}
-          </ul>
+          className={`max-md:hidden uppercase bg-[#E9ECFF] pb-2 text-center flex gap-4 pr-4`}
+        >
+          {navbarItems.map((item) => (
+            <li key={item.href}>
+              <a
+                href={item.href}
+                className={
+                  item.href === "/reserve"
+                    ? "bg-[#808CFD] p-3 rounded-xl [word-spacing:5px]"
+                    : "hover:bg-gray-300 p-2 rounded-lg"
+                }
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
         <div
           className={`md:hidden ${icon}-icon w-18 h-18`}
           onClick={() => setIsMenuOpen((prev) => !prev)}
         />
       </div>
       <ul
-        className={`uppercase bg-[#E9ECFF] pb-2 text-center z-100 ${navbarUlMarginTop} transition-all duration-500 ease-in-out md:hidden`}
+        className={`uppercase bg-[#E9ECFF] text-center pb-3 z-100 ${navbarUlMarginTop} transition-all duration-500 ease-in-out md:hidden`}
       >
         {navbarItems.map((item) => (
-          <li key={item.href}>
-            <a href={item.href}>{item.name}</a>
+          <li key={item.href} className="p-4">
+            <a
+              href={item.href}
+              className={
+                item.href === "/reserve"
+                  ? "bg-[#808CFD] p-3 rounded-xl [word-spacing:5px]"
+                  : "hover:bg-gray-300 p-3 rounded-lg"
+              }
+            >
+              {item.name}
+            </a>
           </li>
         ))}
       </ul>
