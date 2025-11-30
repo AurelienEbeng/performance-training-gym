e63d10e7716a9dcb49bb8c4e6f2082cbb96259e8
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-30 10:38:19 -0500

    Finished about page header section for all devices

--- src/About.tsx
+++ src/About.tsx
@@ -3,28 +3,30 @@ import ScrollFadeIn from "./ScrollFadeIn";
 const About = () => {
   return (
     <>
-    {/* Header section */}
+      {/* Header section */}
       <div className="text-center text-black py-4 border-y headline-one">
         About us
       </div>
-      <ScrollFadeIn style={"w-full md:w-1/3"}>
-        <img
-          src="/images/about/header.jpg"
-          className="w-full md:h-full object-cover"
-          alt="image of people fist bumping"
-        />
-      </ScrollFadeIn>
-      <div className="bg-color-one py-6 px-8">
-        <div className="headline-two pb-10">
-          Tap into Your Primal Power. Forge a Stronger You.
-        </div>
-        <div className="headline-five pb-4">OUR VISION</div>
-        <div className="paragraph-two">
-          Primal Training is committed to delivering a training experience
-          rooted in raw strength, functional fitness, and unwavering community
-          support. We empower our members to tap into their primal power,
-          achieve their goals, and live a life of strength, resilience, and
-          unwavering determination.
+      <div className="md:flex md:flex-row-reverse">
+        <ScrollFadeIn style={"w-full md:w-1/2 lg:w-1/3"}>
+          <img
+            src="/images/about/header.jpg"
+            className="w-full md:h-full object-cover"
+            alt="image of people fist bumping"
+          />
+        </ScrollFadeIn>
+        <div className="bg-color-one py-6 px-8 md:w-1/2 md:px-4 lg:w-2/3 lg:px-8">
+          <div className="headline-two pb-10 lg:pb-40">
+            Tap into Your Primal Power. <span className="lg:block">Forge a Stronger You.</span>
+          </div>
+          <div className="headline-five pb-4">OUR VISION</div>
+          <div className="paragraph-two">
+            Primal Training is committed to delivering a training experience
+            rooted in raw strength, functional fitness, and unwavering community
+            support. We empower our members to tap into their primal power,
+            achieve their goals, and live a life of strength, resilience, and
+            unwavering determination.
+          </div>
         </div>
       </div>
     </>
