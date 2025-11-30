00a3370a46000e678d4ecc67065e4b915073f138
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-30 09:29:01 -0500

    Modified footer padding

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,14 +3,14 @@ import { LINKS } from "./constants";
 
 const Footer = () => {
   return (
-    <footer className="bg-color-two pb-8 px-8">
-      <div className="py-4 md:flex md:justify-between md:pb-40">
+    <footer className="bg-color-two pb-8">
+      <div className="py-4 px-6 md:flex md:justify-between md:pb-40">
         <img src="/vector.svg" className="w-38 h-22" />
         <div className="font-instrument-sans text-5xl font-bold">
           PrimalTraining
         </div>
       </div>
-      <div className="md:flex md:justify-between">
+      <div className="md:flex md:justify-between px-8">
         <div>
           <div className="headline-five pb-4 pt-15 md:pt-0">Contact</div>
           <div className="paragraph-three">Email: hello@figma.com</div>
