5368bfda62df28493119ab0b60ea1bc2375cbb53
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2026-01-24 08:02:02 -0500

    Reservations can be deleted

--- src/ListOfReservations.jsx
+++ src/ListOfReservations.jsx
@@ -9,17 +9,20 @@ const ListOfReservations = () => {
       .then((res) => setReservations(res.data))
       .catch((error) => console.log(error));
   }, []);
+  function handleDelete(id) {
+    httpModule.delete(`delete/${id}/`).catch((error) => console.log(error));
+  }
   return (
     <div>
       {reservations.map((reservation) => (
-        <div className="pb-4">
+        <div className="pb-4" key={reservation.id}>
           <div>Name: {reservation.name}</div>
           <div>Class: {reservation.classType}</div>
           <div>Time: {reservation.time}</div>
           <div>Date: {reservation.date}</div>
           <div className="flex flex-row gap-x-2">
             <button>Update</button>
-            <button>Delete</button>
+            <button onClick={() => handleDelete(reservation.id)}>Delete</button>
           </div>
         </div>
       ))}
