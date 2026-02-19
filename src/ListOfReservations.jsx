de8dde8828e2c5e52ebcb4cd05c98da71c972e57
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2026-02-19 18:25:44 -0500

    Added a table to ListOfReservation component

--- src/ListOfReservations.jsx
+++ src/ListOfReservations.jsx
@@ -23,29 +23,33 @@ const ListOfReservations = () => {
   }
 
   return (
-    <div>
-      <div className="text-center text-black py-4 border-y headline-one">
+    <div className="pb-10">
+      <div className="text-center text-black py-4 headline-one">
         Reservations
       </div>
-      {reservations.map((reservation) => (
-        <>
-          <div className="pb-4" key={reservation.id}>
-            <div>Name: {reservation.name}</div>
-            <div>Class: {reservation.classType}</div>
-            <div>Time: {reservation.time}</div>
-            <div>Date: {reservation.date}</div>
-            <div className="flex flex-row gap-x-2">
-              <button onClick={() => setOpenModal(true)}>Update</button>
-              <button onClick={() => handleDelete(reservation.id)}>
+      <table className="w-7/10 text-center m-auto">
+        <tr>
+          <th className="border">Name</th>
+          <th className="border">Class</th>
+          <th className="border">Time</th>
+          <th className="border">Date</th>
+          <th className="border"></th>
+        </tr>
+        {
+          reservations.map(reservation =>(<tr>
+            <td className="border">{reservation.name}</td>
+            <td className="border">{reservation.classType}</td>
+            <td className="border">{reservation.time}</td>
+            <td className="border">{reservation.date}</td>
+            <td className="border">
+              <button className="w-1/2" onClick={() => setOpenModal(true)}>Update</button>
+              <button className="w-1/2" onClick={() => handleDelete(reservation.id)}>
                 Delete
               </button>
-            </div>
-          </div>
-          <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
-            <UpdateReservation oldReservation={reservation} />
-          </Modal>
-        </>
-      ))}
+            </td>
+          </tr>))
+        }
+      </table>
     </div>
   );
 };
