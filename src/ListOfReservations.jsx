2a5dad732cfbe544fa0fc0b6424019fbaeb8c2dd
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2026-02-19 19:06:37 -0500

    Worked on the table in ListOfReservations

--- src/ListOfReservations.jsx
+++ src/ListOfReservations.jsx
@@ -6,6 +6,7 @@ import UpdateReservation from "./UpdateReservation";
 const ListOfReservations = () => {
   const [reservations, setReservations] = useState([]);
   const [openModal, setOpenModal] = useState(false);
+  const [oldReservation, setOldReservation] = useState({});
 
   useEffect(() => {
     getReservations();
@@ -19,7 +20,10 @@ const ListOfReservations = () => {
   }
 
   function handleDelete(id) {
-    httpModule.delete(`${id}/`).then(getReservations()).catch((error) => console.log(error));
+    httpModule
+      .delete(`${id}/`)
+      .then(getReservations())
+      .catch((error) => console.log(error));
   }
 
   return (
@@ -27,29 +31,50 @@ const ListOfReservations = () => {
       <div className="text-center text-black py-4 headline-one">
         Reservations
       </div>
-      <table className="w-7/10 text-center m-auto">
-        <tr>
-          <th className="border">Name</th>
-          <th className="border">Class</th>
-          <th className="border">Time</th>
-          <th className="border">Date</th>
-          <th className="border"></th>
-        </tr>
-        {
-          reservations.map(reservation =>(<tr>
-            <td className="border">{reservation.name}</td>
-            <td className="border">{reservation.classType}</td>
-            <td className="border">{reservation.time}</td>
-            <td className="border">{reservation.date}</td>
-            <td className="border">
-              <button className="w-1/2" onClick={() => setOpenModal(true)}>Update</button>
-              <button className="w-1/2" onClick={() => handleDelete(reservation.id)}>
-                Delete
-              </button>
-            </td>
-          </tr>))
-        }
+      <table className="w-9/10 text-center m-auto">
+        <thead>
+          <tr>
+            <th className="border">Name</th>
+            <th className="border">Class</th>
+            <th className="border">Time</th>
+            <th className="border">Date</th>
+            <th className="border"></th>
+            <th className="border"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservations.map((reservation) => (
+            <tr>
+              <td className="border">{reservation.name}</td>
+              <td className="border">{reservation.classType}</td>
+              <td className="border">{reservation.time}</td>
+              <td className="border">{reservation.date}</td>
+              <td className="border py-3">
+                <button
+                  className="bg-black text-white rounded-xl px-3"
+                  onClick={() => {
+                    setOpenModal(true);
+                    setOldReservation(reservation);
+                  }}
+                >
+                  Update
+                </button>
+              </td>
+              <td className="border">
+                <button
+                  className="bg-black text-white rounded-xl px-3"
+                  onClick={() => handleDelete(reservation.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+      <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
+        <UpdateReservation oldReservation={oldReservation} />
+      </Modal>
     </div>
   );
 };
