bb21afedb2ad29dd28bc912978610104e565d1af
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2026-02-19 21:06:57 -0500

    When the user clicks x, the modal closes

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -4,12 +4,12 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-12 z-1000 max-w-90' onClick={onClose}>
+    <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-12 z-1000 max-w-90'>
+      <div className='w-full flex justify-end pb-4'><button onClick={onClose} className='mdi--multiply'></button></div>
       <div  
         onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside modal
       >
         {children}
-        <button onClick={onClose}>Close</button>
       </div>
     </div>,
     document.getElementById('modal-root') 
